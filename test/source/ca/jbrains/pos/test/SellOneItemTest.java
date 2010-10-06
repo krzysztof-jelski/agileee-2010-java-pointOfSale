d872bb5b5e92176e552b521862c8fd8e367ac124
J. B. Rainsberger <me@jbrains.ca>
2010-10-06 11:39:39 +0300

    Selling item with different barcodes

--- test/source/ca/jbrains/pos/test/SellOneItemTest.java
+++ test/source/ca/jbrains/pos/test/SellOneItemTest.java
@@ -1,30 +1,62 @@
 package ca.jbrains.pos.test;
 
-import static org.junit.Assert.*;
+import java.util.HashMap;
+import java.util.Map;
+
 import junit.framework.Assert;
 
 import org.junit.Test;
 
-
 public class SellOneItemTest {
+	
 	private static class PointOfSale {
+		private final Screen screen;
+
+		private Map<String, String> storage = new HashMap<String, String>();
+
+		{
+			storage.put("firstBarCode", "$123.50");
+			storage.put("anotherBarCode", "$256.50");
+		}
+
+		public PointOfSale(Screen screen) {
+			this.screen = screen;
+		}
+
 		public void onBarcode(String code) {
+			screen.display(storage.get(code));
 		}
 	}
 
 	private static class Screen {
+		private String price;
+
 		public String getText() {
-			return "$123.50";
+			return price;
+		}
+
+		public void display(String price) {
+			this.price = price;
 		}
 	}
 
 	private Screen screen = new Screen();
+	private PointOfSale pointOfSale = new PointOfSale(screen);
 
 	@Test
 	public void productFound() throws Exception {
-		String code = "randomCode";
-		new PointOfSale().onBarcode(code);
+		String code = "firstBarCode";
+		pointOfSale.onBarcode(code);
 		String price = screen.getText();
 		Assert.assertEquals("$123.50", price);
 	}
+
+	@Test
+	public void productFoundForAnotherBarcode() throws Exception {
+		String code = "anotherBarCode";
+		pointOfSale.onBarcode(code);
+		String price = screen.getText();
+		Assert.assertEquals("$256.50", price);
+	}
+
 }
