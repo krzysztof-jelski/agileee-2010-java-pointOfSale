6c38af2375c7dfbe9ed8beb8db47978179c16279
J. B. Rainsberger <me@jbrains.ca>
2010-10-06 12:13:46 +0300

    Refactor: move responsibility to Screen.

--- test/source/ca/jbrains/pos/test/SellOneItemTest.java
+++ test/source/ca/jbrains/pos/test/SellOneItemTest.java
@@ -10,8 +10,7 @@ import org.junit.Test;
 public class SellOneItemTest {
 	
 
-	private static class PointOfSale {
-		private static final String NO_PRODUCT_FOUND_MESSAGE = "no product found for barcode: %s";
+	static class PointOfSale {
 		private final Screen screen;
 
 		private Map<String, String> storage = new HashMap<String, String>();
@@ -27,28 +26,36 @@ public class SellOneItemTest {
 
 		public void onBarcode(String code) {
 			if (storage.containsKey(code)) {
-				screen.display(storage.get(code));				
+				screen.displayPrice(storage.get(code));				
 			} else if (code.isEmpty()) {
-				screen.display("scanned empty barcode");
+				screen.displayScannedEmptyBarcode();
 			} else {
-				screen.display(noProductFoundMessage(code));
+				screen.displayNoProductFound(code);
 			}
-		}
-
-		private String noProductFoundMessage(String code) {
-			return String.format(NO_PRODUCT_FOUND_MESSAGE, code);
 		}
 	}
 
-	private static class Screen {
-		private String price;
+	static class Screen {
+		private String text;
 
 		public String getText() {
-			return price;
+			return text;
 		}
 
-		public void display(String price) {
-			this.price = price;
+		private void display(String text) {
+			this.text = text;
+		}
+
+		void displayScannedEmptyBarcode() {
+			display("scanned empty barcode");
+		}
+
+		void displayNoProductFound(String code) {
+			display(String.format("no product found for barcode: %s", code));
+		}
+
+		void displayPrice(String price) {
+			display(price);
 		}
 	}
 
@@ -57,29 +64,29 @@ public class SellOneItemTest {
 
 	@Test
 	public void productFound() throws Exception {
-		String code = "firstBarCode";
-		pointOfSale.onBarcode(code);
-		String price = screen.getText();
-		Assert.assertEquals("$123.50", price);
+		pointOfSale.onBarcode("firstBarCode");
+		
+		Assert.assertEquals("$123.50", screen.getText());
 	}
 
 	@Test
 	public void productFoundForAnotherBarcode() throws Exception {
-		String code = "anotherBarCode";
-		pointOfSale.onBarcode(code);
-		String price = screen.getText();
-		Assert.assertEquals("$256.50", price);
+		pointOfSale.onBarcode("anotherBarCode");
+		
+		Assert.assertEquals("$256.50", screen.getText());
 	}
 	
 	@Test
 	public void noProductFound() throws Exception {
 		pointOfSale.onBarcode("unknown barCode");
+		
 		Assert.assertEquals("no product found for barcode: unknown barCode", screen.getText());
 	}
 
 	@Test
 	public void emptyBarcodeReceived() throws Exception {
 		pointOfSale.onBarcode("");
+		
 		Assert.assertEquals("scanned empty barcode", screen.getText());
 	}
 }
